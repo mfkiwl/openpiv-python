Fixed normalization
Modified normalization to correctly represent the zero-normalized cross correlation

openpiv/pyprocess.py
@@ -722,7 +722,7 @@ def fft_correlate_images(
         # longer exposure for frame B
         # image_a = match_histograms(image_a, image_b)
 
-        # remove mean background, normalize to 0..1 range
+        # remove mean, divide by standard deviation
         image_a = normalize_intensity(image_a)
         image_b = normalize_intensity(image_b)
 
@@ -747,8 +747,8 @@ def fft_correlate_images(
         print(f"correlation method {correlation_method } is not implemented")
 
     if normalized_correlation:
-        corr = corr/(s2[0]*s2[1])  # for extended search area
-        corr = np.clip(corr, 0, 1)
+        corr = corr/(corr.shape[-2]*corr.shape[-1])  # for extended search area
+        
     return corr
 
 
@@ -780,7 +780,7 @@ def normalize_intensity(window):
     tmp = window.std(axis=(-2, -1), keepdims=True)
     window = np.divide(window, tmp, out=np.zeros_like(window),
                        where=(tmp != 0))
-    return np.clip(window, 0, window.max())
+    return window
 
 
 def correlate_windows(window_a, window_b, correlation_method="fft",
@@ -825,9 +825,7 @@ def correlate_windows(window_a, window_b, correlation_method="fft",
     It leads to inconsistency of the output
     """
 
-    # first we remove the mean to normalize contrast and intensity
-    # the background level which is take as a mean of the image
-    # is subtracted
+    # remove mean, divide by standard deviation
     # import pdb; pdb.set_trace()
     window_a = normalize_intensity(window_a)
     window_b = normalize_intensity(window_b)
@@ -849,7 +847,7 @@ def correlate_windows(window_a, window_b, correlation_method="fft",
     else:
         print(f"correlation method {correlation_method } is not implemented")
 
-    return corr
+    return corr/(corr.shape[-2]*corr.shape[-1])
 
 
 def fft_correlate_windows(window_a, window_b,